7619966923dc7419f8b106a9ad8a7f3d84af4790
Marah Alsafadi <marahkinana21@gmail.com>
2024-10-30 06:10:34 +0200

    first commit

---
+++ Assets/Scripts/MathematicFile.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MathematicFile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      for(int i = 5;i <= 5; i++){
+    for(int j = 1;j <= 10; j++){
+            Debug.Log(i + "*" + j + "=" + (i*j));
+        
+      }
+
+      }
+    }
+
+ 
+}
