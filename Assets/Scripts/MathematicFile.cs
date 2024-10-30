e5fc9730e625edea3025f31ec71e7c772826163e
Marah Alsafadi <71592330+marah2001@users.noreply.github.com>
2024-10-30 20:20:26 +0200

    adding function

--- Assets/Scripts/MathematicFile.cs
+++ Assets/Scripts/MathematicFile.cs
@@ -7,9 +7,13 @@ public class MathematicFile : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+       int Multiplication(int x , int y) 
+{
+    return  (x * y );
+}
       for(int i = 5;i <= 5; i++){
     for(int j = 1;j <= 10; j++){
-            Debug.Log(i + "*" + j + "=" + (i*j));
+            Debug.Log(i + "*" + j + "=" + Multiplication(i , j));
         
       }
 
